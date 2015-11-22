791a1c3f72c3f036a2738cca6a134663b19342a8
Hai Feng Kao <haifeng@cocoaspice.in>
2015-11-23 03:59:33 +0800

    - kiwi will cause EXC_I386_GPFLT with calls to healStack

--- Example/Pods/Target Support Files/Pods-LeakCanaryiOS_Tests/Pods-LeakCanaryiOS_Tests-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-LeakCanaryiOS_Tests/Pods-LeakCanaryiOS_Tests-frameworks.sh
@@ -85,9 +85,11 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods-LeakCanaryiOS_Tests/LeakCanaryiOS.framework"
-  install_framework "Pods-LeakCanaryiOS_Tests/Kiwi.framework"
+  install_framework "Pods-LeakCanaryiOS_Tests/Expecta.framework"
+  install_framework "Pods-LeakCanaryiOS_Tests/Specta.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods-LeakCanaryiOS_Tests/LeakCanaryiOS.framework"
-  install_framework "Pods-LeakCanaryiOS_Tests/Kiwi.framework"
+  install_framework "Pods-LeakCanaryiOS_Tests/Expecta.framework"
+  install_framework "Pods-LeakCanaryiOS_Tests/Specta.framework"
 fi
--- Example/Tests/LeakCanarySpec.m
+++ Example/Tests/LeakCanarySpec.m
@@ -6,33 +6,44 @@
 //  Copyright (c) 2015 Hai Feng Kao. All rights reserved.
 //
 
-#import <Kiwi/Kiwi.h>
 #import "LeakCanary.h"
+@interface Foo : NSObject
+@end
+@implementation Foo
 
-SPEC_BEGIN(LeakCanarySpec)
+@end
 
-describe(@"LeakCanary", ^{
-    context(@"no leaked objects", ^{
-        it(@"should not detect any objects", ^{
-            [LeakCanary beginSnapShot:@[@"UIVi"]];
-            NSSet* leakedObjects = [LeakCanary endSnapShot];
-            [[@(leakedObjects.count) should] equal:@(0)];
-        });
+SpecBegin(LeakCanarySpec)
+
+context(@"no leaked objects", ^{
+    it(@"should not detect any objects", ^{
+        [LeakCanary beginSnapShot:@[@"UIVi"]];
+        NSSet* leakedObjects = [LeakCanary endSnapShot];
+        expect(leakedObjects).to.beEmpty();
     });
-    context(@"has leaked objects", ^{
-        it(@"should get these objects", ^{
-            @autoreleasepool{
-                [LeakCanary beginSnapShot:@[@"UIVi"]];
-                UIView* dummyView = [UIView new];
-                NSSet* leakedObjects = [LeakCanary endSnapShot];
-                [[@(leakedObjects.count) should] equal:@(1)];
-            }
-            NSSet* leakedObjects = [LeakCanary endSnapShot];
-            [[@(leakedObjects.count) should] equal:@(0)];
-            
-        });
-    });
-
 });
-
-SPEC_END
+context(@"has leaked objects", ^{
+    it(@"should get leaked views", ^{
+        @autoreleasepool{
+            [LeakCanary beginSnapShot:@[@"UIVi"]];
+            UIView* dummyView = [UIView new];
+            NSSet* leakedObjects = [LeakCanary endSnapShot];
+            expect(leakedObjects).to.haveACountOf(1);
+            dummyView = nil;
+        }
+        NSSet* leakedObjects = [LeakCanary endSnapShot];
+        expect(leakedObjects).to.beEmpty();
+    });
+    it(@"should get leaked foos", ^{
+        @autoreleasepool{
+            [LeakCanary beginSnapShot:@[@"Foo"]];
+            Foo* foo = [Foo new];
+            NSSet* leakedObjects = [LeakCanary endSnapShot];
+            expect(leakedObjects).to.haveACountOf(1);
+            foo = nil;
+        }
+        NSSet* leakedObjects = [LeakCanary endSnapShot];
+        expect(leakedObjects).to.beEmpty();
+    });
+});
+SpecEnd
