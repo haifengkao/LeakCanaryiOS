ee3e38bbd206ed9239034289674fa83d8a07025e
Hai Feng Kao <haifeng@cocoaspice.in>
2017-01-03 17:24:03 +0800

    update pods

--- Example/Pods/Target Support Files/LeakCanaryiOS/LeakCanaryiOS-umbrella.h
+++ Example/Pods/Target Support Files/LeakCanaryiOS/LeakCanaryiOS-umbrella.h
@@ -1,7 +1,10 @@
+#ifdef __OBJC__
 #import <UIKit/UIKit.h>
+#endif
 
 #import "HINSPHeapStackInspector.h"
 #import "LeakCanary.h"
+#import "NSObject+HeapInspector.h"
 
 FOUNDATION_EXPORT double LeakCanaryiOSVersionNumber;
 FOUNDATION_EXPORT const unsigned char LeakCanaryiOSVersionString[];
