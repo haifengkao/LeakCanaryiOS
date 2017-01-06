5c60d2a7e349db501a5fb7d782eb8a0ef901e0a3
Hai Feng Kao <haifeng@cocoaspice.in>
2017-01-06 11:47:12 +0800

    weak reference is more robust than malloc_zone_from_ptr

--- Pod/Classes/LeakCanary.m
+++ Pod/Classes/LeakCanary.m
@@ -12,10 +12,32 @@
 #import "NSObject+HeapInspector.h"
 #import <malloc/malloc.h>
 
+@interface LCWeakObject : NSObject
+@property (nonatomic, weak) id object;
+- (instancetype)initWithObject:(id)object NS_DESIGNATED_INITIALIZER;
+@end
+
+@implementation LCWeakObject
+
+- (instancetype)init
+{
+    return [self initWithObject:nil];
+}
+
+- (instancetype)initWithObject:(id)object;
+{
+    if (self = [super init]) {
+        _object = object;
+    }
+    return self;
+}
+@end
+
 @interface LeakCanary()
 @end
 
 static NSSet* liveObjects = nil;
+static NSMutableDictionary* cache = nil;
 @implementation LeakCanary
 
 + (void)beginSnapShot:(NSArray*)array
@@ -24,6 +46,7 @@ static NSSet* liveObjects = nil;
     // to avoid EXE_BAD_ACCESS of zone->introspect->enumerator
     @synchronized(self) {
         liveObjects = nil;
+        cache = [[NSMutableDictionary alloc] init];
         [NSObject addClassPrefixesToRecord:array];
         [HINSPHeapStackInspector performHeapShot];
         [NSObject beginSnapshot];
@@ -51,16 +74,21 @@ static NSSet* liveObjects = nil;
                             //object];
         for (NSString* str in liveObjects) {
             NSRange range = [str rangeOfString:@": "];
-            if (range.location != NSNotFound) {
-                NSString* substr = [str substringFromIndex:range.location + range.length];
-                NSScanner* scanner = [NSScanner scannerWithString:substr];
-                unsigned long long* ptr = nil;
-                
-                [scanner scanHexLongLong:&ptr];
-                if (ptr && NULL == malloc_zone_from_ptr(ptr)) {
-                    [deadObjects addObject:str];
-                } 
+            if (range.location == NSNotFound) { continue; }
+            NSString* substr = [str substringFromIndex:range.location + range.length];
+            LCWeakObject* obj = [cache objectForKey:substr];
+            if (!obj) {
+                id ptr = [HINSPHeapStackInspector objectForPointer:substr];
+                obj = [[LCWeakObject alloc] initWithObject:ptr];
+                cache[substr] = obj;
             } 
+
+            if (!obj) { continue; }  // something went wrong
+
+            if (!obj.object) {
+                // the weak object has been released
+                [deadObjects addObject:str];
+            }
         }
         NSMutableSet* set = [[NSMutableSet alloc] initWithSet:liveObjects];
         [set minusSet:deadObjects];
